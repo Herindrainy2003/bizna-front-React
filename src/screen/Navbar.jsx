345592f9078bbf11d62dd85bd6f71a161b769712
albert <herindrainyall2003@gmail.com>
2024-03-12 19:58:34 +0300

    add logo

--- src/screen/Navbar.jsx
+++ src/screen/Navbar.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div className='header'>
             <div className="header-content">
                 <div className="logo">
-                    <p>SeraSera <span>.</span></p>
+                   <img src="logo.png" alt="" style={{ width: '210px', height: '220px' }} />
                 </div>
                 <button className="hamburger" onClick={toggleMenu}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"/></svg>
